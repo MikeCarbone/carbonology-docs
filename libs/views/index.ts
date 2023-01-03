3e0d28eb42ab64bb980323987ed88f8b013e4c09
Mike Carbone <mfcbone2@gmail.com>
2023-01-02 23:52:50 -0500

    :bug: Fixes fetch url

--- libs/views/index.ts
+++ libs/views/index.ts
@@ -1,8 +1,10 @@
+import { domain } from "../constants/urls";
+
 export const getViews = ({ slug }) => {
-	return fetch(`http://localhost:3000/api/posts/${slug}/views`);
+	return fetch(`${domain}/api/posts/${slug}/views`);
 };
 
 export const logView = ({ slug }) => {
 	if (process.env.NODE_ENV === "development") return;
-	return fetch(`/api/posts/${slug}/views`, { method: "POST" });
+	return fetch(`${domain}/api/posts/${slug}/views`, { method: "POST" });
 };

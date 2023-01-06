d8d2997abd70ad631c93299cd289911b92d4005e
Mike Carbone <mfcbone2@gmail.com>
2023-01-05 22:25:49 -0500

    :lipstick: fixes styling of publication info

--- components/PublicationInfo/PublicationInfo.tsx
+++ components/PublicationInfo/PublicationInfo.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { logView } from "../../libs/views";
 import ViewCount from "../ViewCount";
 import Author from "../Author";
+import style from "./PublicationInfo.module.css";
 
 export const PublicationInfo = ({ slug, publishDate, author, subtitle }) => {
 	useEffect(() => {
@@ -11,35 +12,16 @@ export const PublicationInfo = ({ slug, publishDate, author, subtitle }) => {
 	return (
 		<div style={{ margin: "10px 0 10px 0" }}>
 			{subtitle ? <p>{subtitle}</p> : null}
-			<div
-				style={{
-					display: "flex",
-					margin: "20px 0 0px 0",
-				}}
-			>
-				<ViewCount slug={slug} />
-				<span
-					style={{
-						color: "grey",
-						margin: "0 15px",
-						transform: "scale(1.5) translateY(-2px)",
-					}}
-				>
-					&middot;
-				</span>
-				<p style={{ fontSize: "0.9rem", color: "grey" }}>
-					Published {publishDate}
-				</p>
-				<span
-					style={{
-						color: "grey",
-						margin: "0 15px",
-						transform: "scale(1.5) translateY(-2px)",
-					}}
-				>
-					&middot;
-				</span>
-				<Author author="mike-carbone" />
+			<div className={style.container}>
+				<div className={style.metric}>
+					<ViewCount slug={slug} />
+				</div>
+				<span className={style.separator}>&middot;</span>
+				<p className={style.metric}>Published {publishDate}</p>
+				<span className={style.separator}>&middot;</span>
+				<div className={style.metric}>
+					<Author author="mike-carbone" />
+				</div>
 			</div>
 		</div>
 	);
